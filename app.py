c138bca6bf21f0095108de555e52ac30076573b2
Your Name <you@example.com>
2024-04-14 14:50:38 -0700

    add notes file

--- app.py
+++ app.py
@@ -1,14 +1,3 @@
-# import libraries
-import streamlit as st
-import pandas as pd
-import plotly.express as px
-import plotly.graph_objects as go
-
-
-# Title of the app centered
-st.title('US Vehicle Advertisement Listings')
-
-
 # Read data from csv file vehicled_us_clean.csv
 df = pd.read_csv('./vehicles_us_cleaned.csv')
 
@@ -17,44 +6,33 @@ st.write(df)
 
 # histogram of the types of vehicles by manufacturer
 st.subheader('Histogram of the types of vehicles by manufacturer')
-fig = px.histogram(df, x='Manufacturer', color='Type', y='Type', 
-                   labels={'Type': 'Number of Vehicles'},
-                   title='Number of Vehicles by Manufacturer and Type')
-
+fig = px.histogram(df, x='manufacturer', color='type')
 # plot the histogram
 st.plotly_chart(fig)
 
 # histogram of price distribution between manufacturers
 st.subheader('Histogram of price distribution between manufacturers')
-
 # drop down menu for selecting the manufacturer 1 and 2 
 # index 1 and 2 are used to set default values for the drop down menu
-manufacturer1 = st.selectbox('Manufacturer 1', df['Manufacturer'].unique(), index=1)
-manufacturer2 = st.selectbox('Manufacturer 2', df['Manufacturer'].unique(), index=2)
-
+manufacturer1 = st.selectbox('Manufacturer 1', df['manufacturer'].unique(), index=1)
+manufacturer2 = st.selectbox('Manufacturer 2', df['manufacturer'].unique(), index=2)
 # create a normalized histogram checkbox
 normalized = st.checkbox('Normalized')
-
-# Create a figure object
-fig = go.Figure()
-
-# Add histogram traces for each manufacturer
-fig.add_trace(go.Histogram(x=df[df['Manufacturer'] == manufacturer1]['Price'], name=manufacturer1, opacity=0.75, histnorm='percent'))
-fig.add_trace(go.Histogram(x=df[df['Manufacturer'] == manufacturer2]['Price'], name=manufacturer2, opacity=0.75, histnorm='percent'))
-
+# create a histogram with manufacturer1 and manufacturer2 input
+fig = px.histogram()
+fig.add_trace(go.Histogram(x=df[df['manufacturer'] == manufacturer1]['price'], name=manufacturer1, opacity=0.75, histnorm='percent'))
+fig.add_trace(go.Histogram(x=df[df['manufacturer'] == manufacturer2]['price'], name=manufacturer2, opacity=0.75, histnorm='percent'))
 # normalize the histogram if the checkbox is checked
 if normalized:
     fig.update_layout(barmode='overlay')
     fig.update_traces(opacity=0.75)
-
 # x-axis title
 fig.update_xaxes(title_text='Price')
-
 # y-axis title
 fig.update_yaxes(title_text='Percentage')
-
 # plot the histogram
 st.plotly_chart(fig)
+
 
 
 # scatter plot matrix 
