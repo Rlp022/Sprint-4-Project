bc0bf139a63d6464755fbabb862fc64a477d776b
Rossi Patterson <pattersonr022@gmail.com>
2024-04-18 09:32:26 -0700

    add note files

--- app.py
+++ app.py
@@ -42,10 +42,22 @@ fig = px.histogram(df, x='Condition', y='Price', color='Manufacturer',
                    marginal='rug',  # adds marginal rug plots
                    hover_data=df.columns)
 
-# Check if normalization checkbox is checked
+## create a normalized histogram checkbox
+normalized = st.checkbox('Normalized')
+# create a histogram with manufacturer1 and manufacturer2 input
+fig = px.histogram()
+fig.add_trace(go.Histogram(x=df[df['manufacturer'] == manufacturer1]['price'], name=manufacturer1, opacity=0.75, histnorm='percent'))
+fig.add_trace(go.Histogram(x=df[df['manufacturer'] == manufacturer2]['price'], name=manufacturer2, opacity=0.75, histnorm='percent'))
+# normalize the histogram if the checkbox is checked
 if normalized:
     fig.update_layout(barmode='overlay')
     fig.update_traces(opacity=0.75)
+# x-axis title
+fig.update_xaxes(title_text='Price')
+# y-axis title
+fig.update_yaxes(title_text='Percentage')
+# plot the histogram
+st.plotly_chart(fig)
 
 
 # Set the y-axis limit
